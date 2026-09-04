d0455a5ef6c2acc5a6762e4555e96c6dbd9d8c6e
Jessica Moreno <kenshimota@gmail.com>
2026-09-04 16:33:02 -0400

    agregue le input y un boton

--- lib/modelos/Questions.dart
+++ lib/modelos/Questions.dart
@@ -1,36 +1,37 @@
 class Question {
-  final int? id, answer;
+  final int? id;
   final String? question;
   final List<String>? options;
+  final int? answerIndex;
 
-  Question({this.id, this.question, this.answer, this.options});
+  Question({this.id, this.question, this.options, this.answerIndex});
 }
 
-const List<dynamic> sample_data = <dynamic>[
+const List<dynamic> sampleData = <dynamic>[
   <String, Object>{
     "id": 1,
     "question":
         "Flutter is an open-source UI software development kit created by ______",
     "options": <String>['Apple', 'Google', 'Facebook', 'Microsoft'],
-    "answer_index": 1,
+    "answerIndex": 1,
   },
   <String, Object>{
     "id": 2,
     "question": "When google release Flutter.",
     "options": <String>['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
-    "answer_index": 2,
+    "answerIndex": 2,
   },
   <String, Object>{
     "id": 3,
     "question": "A memory location that holds a single letter or number.",
     "options": <String>['Double', 'Int', 'Char', 'Word'],
-    "answer_index": 2,
+    "answerIndex": 2,
   },
   <String, Object>{
     "id": 4,
     "question": "What command do you use to output data to the screen?",
     "options": <String>['Cin', 'Count>>', 'Cout', 'Output>>'],
-    "answer_index": 2,
+    "answerIndex": 2,
   },
 ];
 
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,16 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/constans.dart';
 
 import 'package:websafe_svg/websafe_svg.dart';
 
+const double kDefaultPadding = 10.0;
+
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Positioned.fill(
-            child: WebsafeSvg.asset('assets/icons/bg.svg', fit: BoxFit.fill),
+          SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: WebsafeSvg.asset('assets/icons/bg.svg', fit: BoxFit.cover),
+          ),
+
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Spacer(flex: 2),
+                  Text(
+                    "Let's Play Quiz,",
+                    style: textTheme.headlineLarge?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Enter your information below"),
+                  Spacer(),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xff1c2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    /* onTap() {
+
+                  },*/
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(kDefaultPadding * 0.75),
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          textStyle: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        child: Text(
+                          "Lets Start Quiz",
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2),
+                ],
+              ),
+            ),
           ),
         ],
       ),
