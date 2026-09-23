cf73cb15e5a496cf4e363917daea51c660fca6ee
Jessica Moreno <kenshimota@gmail.com>
2026-09-23 11:51:34 -0400

    agregue questionController a questionCard

--- lib/screens/quiz/components/question_card.dart
+++ lib/screens/quiz/components/question_card.dart
@@ -1,6 +1,8 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/constans.dart';
 import 'package:quiz_app/screens/quiz/components/option.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
 import 'package:quiz_app/modelos/question.dart';
 
 class QuestionCard extends StatelessWidget {
@@ -10,6 +12,7 @@ class QuestionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
     return Container(
       padding: const EdgeInsets.all(kDefaultPadding),
       decoration: BoxDecoration(
@@ -19,7 +22,7 @@ class QuestionCard extends StatelessWidget {
       child: Column(
         children: [
           Text(
-            question?.question ?? '',
+            question.question ?? '',
             style: Theme.of(
               context,
             ).textTheme.headlineSmall?.copyWith(color: Colors.black),
