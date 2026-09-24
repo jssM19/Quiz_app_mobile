56e7a7f0a53dbb9afbb2780026403e7e56c27d9d
Jessica Moreno <kenshimota@gmail.com>
2026-09-24 16:27:42 -0400

    agregue el widget score_screen a mi archivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/screens/quiz/quiz_screen.dart';
+import 'package:quiz_app/screens/score/score_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: QuizScreen(),
+      home: ScoreScreen(),
     );
   }
 }
