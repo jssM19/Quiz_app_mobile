a76eade77121cc68e1d6223c53097ef350618207
Jessica Moreno <kenshimota@gmail.com>
2026-09-24 16:23:47 -0400

    he creado un widget llamado score_screen.dart

--- lib/screens/quiz/components/option.dart
+++ lib/screens/quiz/components/option.dart
@@ -43,7 +43,7 @@ class Option extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "{$index + 1}. $text",
+                  "$index. $text",
                   style: TextStyle(color: getTheRightColor(), fontSize: 16),
                 ),
                 Container(
--- lib/screens/quiz/quiz_screen.dart
+++ lib/screens/quiz/quiz_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/screens/quiz/components/body.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
+import 'package:get/get.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({Key? key});
   @override
   Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
